2cef86eea5835d1047c56abf9a0846ea4ce50324
Elwyn Harazaki <Harazaki@MGXA2EHA.local>
2017-10-24 21:24:21 +0700

    Updated

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -13,7 +13,7 @@ const CardSection = (props) => {
 
 const styles = {
    cardSectionStyle: {
-      borderBottomWidth: 1,
+      borderBottomWidth: 2,
       padding: 5,
       backgroundColor: '#0C508B',
       justifyContent: 'flex-start',
